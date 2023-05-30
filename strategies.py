09c95c00fc61f0221132d1013ed137cc0af02adf
John Taylor <johntaylor@google.com>
2023-05-30 14:48:02 -0400

    Adds some logic to detect the presence of a winning move.

--- strategies.py
+++ strategies.py
@@ -17,6 +17,54 @@ class CpuRandomStrategy(Strategy):
 
 
 class CpuBetterStrategy(Strategy):
+  def check_for_winning_move(self, symbol):
+    """Check the board to see if there's a winning move available for the given
+    symbol.
+
+    If there's a winning move return the number corresponding to it.
+    Returns None if there's no winning move.
+
+    symbol - either an X or a O
+    """
+    # Check to see if there's a complete row
+    for row in range(0, 3):
+      symbol_count = 0
+      empty_col_index = None
+      for col in range(0, 3):
+        if board[row][col] == symbol:
+          symbol_count += 1
+        elif board[row][col] in range(0, 10):
+          empty_col_index = board[row][col]
+      if symbol_count == 2:
+        # We have a winning move!
+        return empty_col_index
+
+    # Now do the same for a winning column.
+
+
+
+
+
+    # Cross wins
+    symbol = board[1][1]
+    if (symbol in ['X', 'O'] and board[0][0] == symbol and board[2][
+      2] == symbol):
+      return symbol
+    elif (symbol in ['X', 'O'] and board[0][2] == symbol and board[2][
+      0] == symbol):
+      return symbol
+
+    # board is full
+    for row in range(0, 3):
+      for collom in range(0, 3):
+        symbol = board[row][collom]
+        # print(f"Symbol: {symbol}")
+        if symbol not in ['X', 'O']:
+          return None
+    return "N"
+
+
+
   def make_turn(self):
     # TODO: Update this
     cpu_choice = random.choice(list(board.possibleNumbers))
